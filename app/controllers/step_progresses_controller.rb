75dde951dd1f08824663466e36c88282b446f484
Kseniia Novinskaia <kseniia.novinskaia@gmail.com>
2021-02-20 16:24:49 +0100

    Create my step progresses for the chosen path

--- app/controllers/step_progresses_controller.rb
+++ app/controllers/step_progresses_controller.rb
@@ -1,5 +1,13 @@
 class StepProgressesController < ApplicationController
   def create
+    @path = Path.find(params[:path_id])
+    @path.steps.each do |step|
+      @step_progress = StepProgress.new
+      @step_progress.path = @path
+      @step_progress.user = current_user
+      @step_progress.step = step
+      @step_progress.save
+    end
   end
 
   def update
