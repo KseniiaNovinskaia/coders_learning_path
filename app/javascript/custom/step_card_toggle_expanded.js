73affdb5ea24823e59fcf53bc47c33dc307d77ff
Philipp Schaefer <pschaefer@posteo.net>
2021-02-26 23:17:20 +0100

    dynamic progress-dependent behavior on step page + progress bar + front end

--- app/javascript/custom/step_card_toggle_expanded.js
+++ app/javascript/custom/step_card_toggle_expanded.js
@@ -4,18 +4,18 @@ const initStepCardEventListener = () => {
 
   for (var i = 0; i < stepCards.length; i++) {
     let stepCard = stepCards[i];
-    // console.log(stepCard);
     let closedPart = stepCard.getElementsByClassName('step-card-closed-part')[0];
     console.log(closedPart);
     closedPart.addEventListener('click', (event) => {
 
       let expandedPart = stepCard.getElementsByClassName('step-card-expanded-part')[0];
       expandedPart.classList.toggle('step-card-expanded-part-expanded');
-        // console.log(expandedPart);
 
       let expandIcon = stepCard.getElementsByClassName('step-card-expand-icon')[0];
-      console.log(expandIcon);
       expandIcon.classList.toggle('step-card-expand-icon-expanded');
+
+      let closedPart = stepCard.getElementsByClassName('step-card-closed-part')[0];
+      closedPart.classList.toggle('step-card-closed-part-expanded');
   });
   }
 };
