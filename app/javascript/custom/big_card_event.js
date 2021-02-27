e5f2a5b03474677737f9b970c6f9bae861e56e7e
sabina barcucci <me@sabinabar.cc>
2021-02-27 17:07:42 +0100

    adding advancement on expanded card in index page

---
+++ app/javascript/custom/big_card_event.js
@@ -0,0 +1,17 @@
+const littleCards = document.querySelectorAll(".p_card_sm");
+const bigCards = document.querySelectorAll(".path-card-expanded");
+littleCards.forEach(littleCard => {
+  littleCard.addEventListener("click", (event) => {
+    // Find the big card id from the little card data attributes
+    const bigCardId = littleCard.dataset.bigCardId;
+    // Find the big card element from the big card id
+    const bigCard = document.getElementById(bigCardId);
+    // Hide all existing big cards
+    bigCards.forEach(bigCard => {
+      console.log("hiding card...");
+      bigCard.classList.add("d-none")
+    });
+    // Display the selected big card
+    bigCard.classList.remove("d-none");
+  })
+})
