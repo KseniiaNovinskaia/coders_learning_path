1fe28b2cf46112464722e391c5f5458870db8d6a
Philipp Schaefer <pschaefer@posteo.net>
2021-02-27 13:50:30 +0100

    enrich seeds (not done yet); move duration calculation from paths controller to step model

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,4 +1,20 @@
 class Step < ApplicationRecord
   belongs_to :path
   has_many :step_progresses
+
+  def duration_string
+    duration = self.duration
+    hours = duration / 60
+    minutes = duration % 60
+    duration_string = ""
+    duration_string += "#{hours}h" unless hours.zero?
+    duration_string += " #{minutes}m" unless minutes.zero?
+    duration_string
+  end
+
+  def external_source_icon
+    # ToDo: return matching logo based on step's type
+  end
+
+
 end
